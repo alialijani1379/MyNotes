aab82ceda67392338efae506529af2869d01a9bb
Ali <ah.alijani.1379@gmail.com>
2021-03-02 22:17:31 +0330

    new:...

--- app/src/main/java/com/example/notes/activity/MainActivity.java
+++ app/src/main/java/com/example/notes/activity/MainActivity.java
@@ -111,7 +111,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 filterList.add(model);
             }
         }
-
         adapter.filterList(filterList);
     }
 
