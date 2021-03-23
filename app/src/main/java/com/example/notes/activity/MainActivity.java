8b668a348ea57526a6c52fafb74583fe49f4b82d
Ali <ah.alijani.1379@gmail.com>
2021-03-23 13:58:56 +0430

    new:...

--- app/src/main/java/com/example/notes/activity/MainActivity.java
+++ app/src/main/java/com/example/notes/activity/MainActivity.java
@@ -46,7 +46,6 @@ import io.supercharge.shimmerlayout.ShimmerLayout;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, ListenerUpdate, ListenerDelete {
     // Happy new year
-
     //<editor-fold desc="--Declaration--">
     public static final String TITLE_U = "title_u";
     public static final String SUBTITLE_U = "subtitle_u";
